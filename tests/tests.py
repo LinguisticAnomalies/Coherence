5cc6e7a66edfe6ecadfae96ee609de1d8b10ab46
Yongsen Tan <tan2770943001@gmail.com>
2026-08-11 21:08:32 -0700

    chore: remove sys path & save pkl to a folder

--- tests/tests.py
+++ tests/tests.py
@@ -2,30 +2,34 @@
 # testing in general, but rather to support the `find_packages` example in
 # setup.py that excludes installing the "tests" package
 
-import sys
-sys.path.append('/edata/coherencenotebook/coherencecalculator/src')
+import os
 from coherencecalculator.pipelines.timeseries import timeseries
 from coherencecalculator.pipelines.features import features
 from coherencecalculator.pipelines.tardis import tardis
 from coherencecalculator.pipelines.agg import agg
 from coherencecalculator.tools.vecloader import VecLoader
 import pandas as pd
-import pickle
+
 
 if __name__ == '__main__':
-    inputDir = '/edata/coherencenotebook/coherencecalculator/tests/testData'
+    testsDir = os.path.dirname(os.path.abspath(__file__))
+    inputDir = os.path.join(testsDir, 'testData')
+    if not os.path.isdir(inputDir):
+        raise FileNotFoundError(f'Input data directory not found: {inputDir}')
     vecs = VecLoader()
+    outputDir = os.path.join(testsDir, 'output')
+    os.makedirs(outputDir, exist_ok=True)
     #If data is csv/pkl/dataframe, file column name and text column name need to be added
-    #For example: 
+    #For example:
     # tsDf = timeseries(vecLoader=vecs, inputCsv=path_to_csv, fileCol='IDCOLNAME', textCol='TEXTCOLNAME', saveDir='./timeseriesout.pkl')
-    tsDf = timeseries(vecLoader=vecs, inputDir=inputDir, saveDir='./timeseriesout.pkl')
-    print(pd.read_pickle('./timeseriesout.pkl'))
-    
-    featureDict = features(vecLoader=vecs, inputTimeseries=tsDf, saveDir='./featuresout.pkl')
+    tsDf = timeseries(vecLoader=vecs, inputDir=inputDir, saveDir=os.path.join(outputDir, 'timeseriesout.pkl'))
+    print(pd.read_pickle(os.path.join(outputDir, 'timeseriesout.pkl')))
+
+    featureDict = features(vecLoader=vecs, inputTimeseries=tsDf, saveDir=os.path.join(outputDir, 'featuresout.pkl'))
     print(featureDict)
-    
-    tardis(vecLoader=vecs, inputFeatures=featureDict, saveDir='./tardisout.pkl')
-    print(pd.read_pickle('./tardisout.pkl'))
-    
-    agg(vecLoader=vecs, inputTimeseries=tsDf, saveDir='./aggout.pkl')
-    print(pd.read_pickle('./aggout.pkl'))
+
+    tardis(vecLoader=vecs, inputFeatures=featureDict, saveDir=os.path.join(outputDir, 'tardisout.pkl'))
+    print(pd.read_pickle(os.path.join(outputDir, 'tardisout.pkl')))
+
+    agg(vecLoader=vecs, inputTimeseries=tsDf, saveDir=os.path.join(outputDir, 'aggout.pkl'))
+    print(pd.read_pickle(os.path.join(outputDir, 'aggout.pkl')))
